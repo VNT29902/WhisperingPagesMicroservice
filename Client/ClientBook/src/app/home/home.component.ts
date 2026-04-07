547b865f4d0ffea73525c8794a5aff0089bb6eac
VNT29902 <108218603+VNT29902@users.noreply.github.com>
2026-04-07 21:47:06 +0700

    feat(home): redesign ecommerce hero and promo entry layout

--- Client/ClientBook/src/app/home/home.component.ts
+++ Client/ClientBook/src/app/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { HeaderComponent } from '../header/header.component';
 import { BestsellingComponent } from '../bestselling/bestselling.component';
 import { PopularbookComponent } from '../popularbook/popularbook.component';
@@ -11,6 +11,12 @@ import { catchError, map, of } from 'rxjs';
 import { LatestReleaseBooksComponent } from '../latest-release-books/latest-release-books.component';
 import { CommonModule } from '@angular/common';
 import { RouterLink } from '@angular/router';
+
+type PromoItem = {
+  image: string;
+  label?: string;
+  link: string;
+};
 
 @Component({
   selector: 'app-home',
@@ -28,7 +34,7 @@ import { RouterLink } from '@angular/router';
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css'],
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, OnDestroy {
   latestBooks: BookAndPromotion[] = [];
   bestsellingBooks: BookAndPromotion[] = [];
   categoryBooks: BookAndPromotion[] = [];
@@ -36,10 +42,47 @@ export class HomeComponent implements OnInit {
   featuredBooks: BookAndPromotion[] = [];
   partialLoadFailed = false;
   loadFailed = false;
+  currentHeroSlide = 0;
+  private heroIntervalId: ReturnType<typeof setInterval> | null = null;
+
+  readonly heroSlides: PromoItem[] = [
+    { image: 'https://cdn1.fahasa.com/media/magentothem/banner7/trangCTT4_1504_840x320.png', link: '/products' },
+    { image: 'https://cdn1.fahasa.com/media/magentothem/banner7/AzVietNamt4_resize_840x320.png', link: '/products' },
+    { image: 'https://cdn1.fahasa.com/media/magentothem/banner7/TrangUuDaiT3_Resize_840x320.png', link: '/products' },
+    { image: 'https://cdn1.fahasa.com/media/magentothem/banner7/LDP_SachThamKhao_840x320.png', link: '/products' },
+    { image: 'https://cdn1.fahasa.com/media/magentothem/banner7/megabook_t4_840x320.png', link: '/products' },
+  ];
+
+  readonly rightHeroPromos: PromoItem[] = [
+    { image: 'https://cdn1.fahasa.com/media/wysiwyg/Thang-04-2026/Fahasa_392x156.jpg', link: '/products' },
+    { image: 'https://cdn1.fahasa.com/media/magentothem/banner7/TrangUuDaiT3_Resize_840x320.png', link: '/products' },
+  ];
+
+  readonly secondaryPromos: PromoItem[] = [
+    { image: 'https://cdn1.fahasa.com/media/wysiwyg/Thang-04-2026/Deli_resize_310X210.png', link: '/products' },
+    { image: 'https://cdn1.fahasa.com/media/wysiwyg/Thang-04-2026/NgoaiVanT4_Resize_310x210.png', link: '/products' },
+    { image: 'https://cdn1.fahasa.com/media/wysiwyg/Thang-04-2026/MinhLongBooks_Resize_310x210.png', link: '/products' },
+    { image: 'https://cdn1.fahasa.com/media/wysiwyg/Thang-04-2026/trangvanhoc_t4_310x210.png', link: '/products' },
+  ];
+
+  readonly quickEntries: PromoItem[] = [
+    { image: 'https://cdn1.fahasa.com/media/wysiwyg/Thang-04-2026/Icon_1504_120x120_1.png', label: '15.04', link: '/products' },
+    { image: 'https://cdn1.fahasa.com/media/wysiwyg/Thang-02-2026/Icon_Sale50_120x120.png', label: 'Ưu Đãi Siêu To', link: '/products' },
+    { image: 'https://cdn1.fahasa.com/media/wysiwyg/Thang-01-2025/IconFlashSale120x120.png', label: 'Flash Sale', link: '/products' },
+    { image: 'https://cdn1.fahasa.com/media/wysiwyg/icon-menu/Icon_MaGiamGia_8px_1.png', label: 'Mã Giảm Giá', link: '/products' },
+    { image: 'https://cdn1.fahasa.com/media/wysiwyg/Thang-04-2025/Icon_AlphaBooks_120x120.png', label: 'Alpha Books', link: '/products' },
+    { image: 'https://cdn1.fahasa.com/media/wysiwyg/Thang-06-2025/Icon_TanViet_120x120.png', label: 'Tân Việt', link: '/products' },
+    { image: 'https://cdn1.fahasa.com/media/wysiwyg/icon-menu/Icon_SanPhamMoi_8px_1.png', label: 'Sản Phẩm Mới', link: '/products/latest' },
+    { image: 'https://cdn1.fahasa.com/media/wysiwyg/Thang-01-2024/ChoDoCu.png', label: 'Phiên Chợ Đồ Cũ', link: '/products' },
+    { image: 'https://cdn1.fahasa.com/media/wysiwyg/Thang-03-2026/IconNgoaiVan120x120.png', label: 'Ngoại Văn', link: '/products' },
+    { image: 'https://cdn1.fahasa.com/media/wysiwyg/Thang-06-2024/icon_ManngaT06.png', label: 'Manga', link: '/products' },
+  ];
 
   constructor(private bookService: BookService) {}
 
   ngOnInit(): void {
+    this.startHeroAutoplay();
+
     forkJoin({
       latest: this.bookService.getLatestBooks().pipe(
         map((data) => ({ data: data || [], failed: false })),
@@ -79,6 +122,31 @@ export class HomeComponent implements OnInit {
         this.loadFailed = true;
       },
     });
+  }
+
+  ngOnDestroy(): void {
+    if (this.heroIntervalId) {
+      clearInterval(this.heroIntervalId);
+      this.heroIntervalId = null;
+    }
+  }
+
+  nextHeroSlide(): void {
+    this.currentHeroSlide = (this.currentHeroSlide + 1) % this.heroSlides.length;
+  }
+
+  previousHeroSlide(): void {
+    this.currentHeroSlide = (this.currentHeroSlide - 1 + this.heroSlides.length) % this.heroSlides.length;
+  }
+
+  goToHeroSlide(index: number): void {
+    this.currentHeroSlide = index;
+  }
+
+  private startHeroAutoplay(): void {
+    this.heroIntervalId = setInterval(() => {
+      this.nextHeroSlide();
+    }, 4500);
   }
 
   getDiscountPrice(book: BookAndPromotion): number {
